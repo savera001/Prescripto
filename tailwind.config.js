4633e7e2c93f4aee0815efa99e635103917b6f44
savera001 <130281935+savera001@users.noreply.github.com>
2025-05-10 21:07:38 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors:{
+        'primary' : "#5f6FFF"
+      },
+      gridTemplateColumns:{
+        'auto':'repeat(auto-fill, minmax(200px,1fr))'
+      }
+    },
+  },
+  plugins: [],
+}
